0cc1e276cfc52679c37e690ed7f8b78f9078c852
Jason Zurita <jasonzurita@me.com>
2016-11-09 08:47:48 -0500

    possible first addition of force touch recognizer

---
+++ Baby Tracker/Tile.swift
@@ -0,0 +1,26 @@
+//
+//  Tile.swift
+//  Baby Tracker
+//
+//  Created by Jason Zurita on 11/9/16.
+//  Copyright © 2016 Jason Zurita. All rights reserved.
+//
+
+import UIKit
+
+class Tile: UIButton {
+
+    let shouldPrintDebugString = true //set to false to silence this class
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            printDebugString(string: "Touch pressure: \(touch.force/touch.maximumPossibleForce)")
+        }
+    }
+
+    private func printDebugString(string:String) {
+        if shouldPrintDebugString {
+            print("\(type(of:self)): " + string)
+        }
+    }
+}
