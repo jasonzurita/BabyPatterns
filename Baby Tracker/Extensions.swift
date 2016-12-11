ae1e7f0de7233a6eb6d46d866a75e08730684704
Jason Zurita <jasonzurita@me.com>
2016-12-11 15:05:56 -0500

    creates UIView extension helper to load a given view’s nib file

--- Baby Tracker/Extensions.swift
+++ Baby Tracker/Extensions.swift
@@ -7,11 +7,18 @@
 //
 
 import Foundation
+import UIKit
 
 extension Date {
     
     init?(_ string:String?) {
         guard let s = string, let interval = TimeInterval(s) else { return nil }
         self = Date(timeIntervalSince1970: interval)
+    }
+}
+
+extension UIView {
+    func loadNib() {
+        Bundle.main.loadNibNamed(String(describing: type(of:self)), owner: self, options: nil)
     }
 }
