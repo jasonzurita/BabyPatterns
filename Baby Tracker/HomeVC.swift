d2a2aff2ebfffd45ddc9e55b95b6813752fac60e
Jason Zurita <jasonzurita@me.com>
2016-12-15 18:10:39 -0500

    first pass at making profile view

--- Baby Tracker/HomeVC.swift
+++ Baby Tracker/HomeVC.swift
@@ -18,17 +18,9 @@ class HomeVC: UIViewController {
     //TODO: okay for for now, put these into a collectoin view to easily support future tile additions
     @IBOutlet weak var feedingTile: Tile!
     @IBOutlet weak var changingsTile: Tile!
-    @IBOutlet weak var babyNameLabel: UILabel!
-    @IBOutlet weak var babyAgeLabel: UILabel!
-    @IBOutlet weak var todaysDateLabel: UILabel!
-    @IBOutlet weak var babysProfilePictureView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        babyNameLabel.text = "James"
-        babyAgeLabel.text = "7 Months old"
-        todaysDateLabel.text = DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .none)
-
         loadFeedingData()
         setupTileListeners()
     }
---
+++ Baby Tracker/ProfileView.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileView.swift
+//  Baby Tracker
+//
+//  Created by Jason Zurita on 12/15/16.
+//  Copyright © 2016 Jason Zurita. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    //outlets
+    @IBOutlet var view: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+
+    override init(frame:CGRect) {
+        super.init(frame:frame)
+        initializeView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initializeView()
+    }
+    
+    private func initializeView() {
+        loadNib()
+        view.frame = bounds
+        addSubview(view)
+    }
+}
