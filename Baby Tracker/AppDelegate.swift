d8b72ebfb67e58acb277461c816974ed2987e52c
Jason Zurita <jasonzurita@me.com>
2017-01-02 19:08:07 -0500

    enables offline persistence

--- Baby Tracker/AppDelegate.swift
+++ Baby Tracker/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FIRApp.configure()
+        FIRDatabase.database().persistenceEnabled = true
         return true
     }
 
