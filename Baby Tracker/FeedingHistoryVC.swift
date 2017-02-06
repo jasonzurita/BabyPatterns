43ecf7ede6cb9e3490f2143e8977ddaa10e292d3
Jason Zurita <jasonzurita@me.com>
2017-02-06 09:54:35 -0500

    properly displays all feeding events in scroll view (on a 24 hours scale) (UI cleanup needed)

--- Baby Tracker/FeedingHistoryVC.swift
+++ Baby Tracker/FeedingHistoryVC.swift
@@ -10,9 +10,6 @@ import UIKit
 
 class FeedingHistoryVC: UIViewController {
     
-    weak var feedings:FeedingVM?
-    
-    @IBOutlet weak var scrollView: UIScrollView!
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .landscape
     }
@@ -22,40 +19,75 @@ class FeedingHistoryVC: UIViewController {
     }
     
     private var notificationToken:NSObjectProtocol?
+    private let screenTimeWindowSeconds:TimeInterval = 24 * 60 * 60
+    private let screenHeight = UIScreen.main.bounds.size.width
+    private let screenWidth = UIScreen.main.bounds.size.height
+    private var barGraphHeight:CGFloat {
+        return screenHeight * 0.5
+    }
+    private let barGraphYOffset:CGFloat = 10
+    private let barGraphElementWidth:CGFloat = 6
     
+    private var pointsPerSecond:CGFloat {
+        return screenWidth / CGFloat(screenTimeWindowSeconds)
+    }
+
+    weak var feedings:FeedingVM?
+
+    @IBOutlet weak var scrollView: UIScrollView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let feedings = feedings?.feedingsMatching(type: .nursing, isFinished: true) else { return }
-        loadFeedings(feedings:feedings)
+        setupGraph()
     }
     
-    private func loadFeedings(feedings:[Feeding]) {
+    private func setupGraph() {
+        guard let allFeedings = feedings?.feedingsMatching(type: .nursing, isFinished: true) else {
+            print("no feedings to show...")
+            return }
+
+        let fullFeedingsWindow = dateIntervalWindow(endDate:allFeedings.first?.endDate)
+        adjustScrollViewContentSize(width: CGFloat(abs(fullFeedingsWindow.start.timeIntervalSinceNow)) * pointsPerSecond)
+        layoutFeedings(allFeedings, inWindow: fullFeedingsWindow)
+        adjustScrollViewContentOffset()
+    }
+    
+    private func dateIntervalWindow(endDate:Date?) -> DateInterval {
         let now = Date() //when? now! when now? now now!
-        let past = Date(timeInterval: -(24 * 60 * 60), since: now)
-        let window = DateInterval(start: past, end: now)
-        
-        let screenHeight = UIScreen.main.bounds.size.width
-        let barGraphHeight = screenHeight * 0.5
-        
+        let past = endDate ?? Date(timeInterval: -(screenTimeWindowSeconds), since: now)
+        return DateInterval(start: past, end: now)
+    }
+    
+    private func adjustScrollViewContentSize(width:CGFloat) {
+        scrollView.contentSize = CGSize(width: width, height: scrollView.contentSize.height)
+    }
+    
+    private func layoutFeedings(_ feedings:[Feeding], inWindow window:DateInterval) {
         for feeding in feedings {
-            guard let endDate = feeding.endDate else { return }
-            let x = xLocation(forDate: endDate, inWindow: window)
-            let frame = CGRect(x: x, y: screenHeight - (barGraphHeight + 10), width: 8, height: barGraphHeight)
+            guard let endDate = feeding.endDate else {
+                print("Feeding not finished, so cannot display...")
+                return
+            }
+            let x = xFeedingLocation(forDate: endDate, inWindow: window)
+            let frame = CGRect(x: x, y: screenHeight - (barGraphHeight + barGraphYOffset), width: barGraphElementWidth, height: barGraphHeight)
             let graphElement = BarGraphLollipop(frame: frame)
+            
             scrollView.addSubview(graphElement)
         }
     }
     
-    private func xLocation(forDate date:Date, inWindow window:DateInterval) -> CGFloat {
-        let screenWidth = UIScreen.main.bounds.size.height
-
-        let pointsPerSecond = screenWidth / CGFloat(window.duration)
-        let secondsSinceNow = abs(date.timeIntervalSince(window.end))
+    private func xFeedingLocation(forDate date:Date, inWindow window:DateInterval) -> CGFloat {
+        let maxX = scrollView.contentSize.width
+        let secondsSinceNow = CGFloat(abs(date.timeIntervalSince(window.end)))
         
-        return pointsPerSecond * CGFloat(secondsSinceNow)
+        return maxX - pointsPerSecond * CGFloat(secondsSinceNow) - barGraphElementWidth
     }
-
+    
+    private func adjustScrollViewContentOffset() {
+        scrollView.contentOffset = CGPoint(x: scrollView.contentSize.width, y: scrollView.contentOffset.y)
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         UIDevice.current.endGeneratingDeviceOrientationNotifications()
@@ -75,7 +107,7 @@ class FeedingHistoryVC: UIViewController {
                 UIDevice.current.endGeneratingDeviceOrientationNotifications()
                 center.removeObserver(self.notificationToken!)
             }
-
+            
         })
     }
 }
