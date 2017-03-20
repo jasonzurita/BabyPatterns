aeeb0d44aa016c07c8034fe0f85e89a77c7148d4
Jason Zurita <jasonzurita@me.com>
2017-03-20 17:47:45 -0400

    first cut at updating bottle feeding view

--- Baby Tracker/BottleVC.swift
+++ Baby Tracker/BottleVC.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class BottleVC: UIViewController {
+    fileprivate let shouldPrintDebugString = true
+    
     var feedingType:FeedingType = .bottle
+    
+    @IBOutlet weak var feedingAmountWholeNumberPickerView: UIPickerView!
+    @IBOutlet weak var feedingAmountFractionPickerView: UIPickerView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,16 +27,24 @@ class BottleVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension BottleVC: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        Logger.log(message: "Row selected", object: self, type: .info, shouldPrintDebugLog: shouldPrintDebugString)
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(row)"
+    }
+}
 
+extension BottleVC: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 10
+    }
 }
