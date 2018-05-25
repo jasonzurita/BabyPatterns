cc2643bd5ca9a0187e97f6eac905b60430ad3f76
Jason Zurita <jasonzurita@me.com>
2018-05-25 08:31:21 -0400

    Add color pallet UIColor extension

---
+++ Framework-BabyPatterns/Extensions/Color.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+extension UIColor {
+    static var bpDarkBlue: UIColor {
+        // #0055CC
+        return UIColor(red: 0.0 / 255.0, green: 85.0 / 255.0, blue: 204.0 / 255.0, alpha: 1.0)
+    }
+    static var bpMediumBlue: UIColor {
+        // #0099FF
+        return UIColor(red: 0.0 / 255.0, green: 153.0 / 255.0, blue: 225.0 / 255.0, alpha: 1.0)
+    }
+    static var bpLightBlue: UIColor {
+        // #66CCFF
+        return UIColor(red: 102.0 / 255.0, green: 204.0 / 255.0, blue: 225.0 / 255.0, alpha: 1.0)
+    }
+
+    static var bpDarkGray: UIColor {
+        // #333333
+        return UIColor(red: 51.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0)
+    }
+    static var bpMediumGray: UIColor {
+        // #999999
+        return UIColor(red: 153.0 / 255.0, green: 153.0 / 255.0, blue: 153.0 / 255.0, alpha: 1.0)
+    }
+    static var bpLightGray: UIColor {
+        // #CCCCCC
+        return UIColor(red: 204.0 / 255.0, green: 204.0 / 255.0, blue: 204.0 / 255.0, alpha: 1.0)
+    }
+
+    static var bpGreen: UIColor {
+        // #00CC11
+        return UIColor(red: 0.0 / 255.0, green: 204.0 / 255.0, blue: 17.0 / 255.0, alpha: 1.0)
+    }
+
+    static var bpPink: UIColor {
+        // #FF7FCF
+        return UIColor(red: 255.0 / 255.0, green: 127.0 / 255.0, blue: 207.0 / 255.0, alpha: 1.0)
+    }
+
+    static var bpWhite: UIColor {
+        // #FFFFFF
+        return UIColor(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+    }
+}
