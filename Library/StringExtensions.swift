f33308a020c25b78657e3a133f41e31a3c40425f
Jason Zurita <jasonzurita@me.com>
2018-06-02 12:53:35 -0400

    Add to do for future consideration

--- Library/StringExtensions.swift
+++ Library/StringExtensions.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+// TODO: consider making a new type that is a facade for String
+// like validatableString, and extend that. This wouldn't globablly
+// make string validatable, which is a domain specific extension.
 public extension String {
     func validateEmail() -> Bool {
         return true
