5d59b4fbea001f03df3c24d210d00571628f97bd
Jason Zurita <jasonzurita@me.com>
2018-06-04 23:30:02 -0400

    Add basic turn off ads view controller

---
+++ Framework-BabyPatterns/TurnOffAdsVC.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+public final class TurnOffAdsVC: UIViewController {
+    public init() {
+        super.init(nibName: "\(type(of: self))", bundle: Bundle(for: type(of: self)))
+    }
+
+    public required init?(coder _: NSCoder) { fatalError("\(#function) has not been implemented") }
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func dismissButtonTapped(_: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
