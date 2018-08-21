4a811b9be04aa283c52d279807167cd2c98dd05c
Jason Zurita <jasonzurita@me.com>
2018-08-21 17:40:58 -0400

    Add a simple tappable view

---
+++ Library/TapableView.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+public final class TapableView: UIView {
+
+    public var onTap: ((UIView) -> Void)?
+
+    public init() {
+        super.init(frame: .zero)
+        let tgr = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        addGestureRecognizer(tgr)
+    }
+
+    public required init?(coder aDecoder: NSCoder) { fatalError("\(#function) has not been implemented") }
+
+    @objc func tapped() { onTap?(self) }
+}
