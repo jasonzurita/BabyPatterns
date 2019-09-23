88faa7ed6fda8f7930d4d1ed8b81419851ee846e
Jason Zurita <jasonzurita@me.com>
2019-09-23 13:41:56 -0400

    feat(watch): add stock apple watch extension
    Slightly modified from the Xcode provided watch
    template to clean up thing like comments.
    
    Start of the watch companion (maybe stand alone)
    watch app!!!

---
+++ Watch Extension/HostingController.swift
@@ -0,0 +1,9 @@
+import WatchKit
+import Foundation
+import SwiftUI
+
+final class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
