4026fc14f0f7e47d6bccf8704366040fbb827dd8
Jason Zurita <jasonzurita@me.com>
2019-10-22 17:25:09 -0400

    chore: update todos
    This appears to be working pretty well!

--- Watch Extension/HomeView.swift
+++ Watch Extension/HomeView.swift
@@ -4,8 +4,6 @@ import SwiftUI
  TODO: - V1 Watch
  - Polish UI
  - Handle UI when main app manages an in-progress feeding
- - Make sure a backgrounded/terminated main app
-   behaves well from a UI standpoint when using the watch
  - Think about subscriptions or IAP for this
  - Load all feedings when starting up?
  */
