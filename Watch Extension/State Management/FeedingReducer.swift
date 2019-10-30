87c79936a1e128028c002cebd2b0360912696796
Jason Zurita <jasonzurita@me.com>
2019-10-30 07:32:05 -0400

    chore: clarify todo

--- Watch Extension/State Management/FeedingReducer.swift
+++ Watch Extension/State Management/FeedingReducer.swift
@@ -9,6 +9,7 @@ func feedingReducer(value: inout [Feeding], action: FeedingAction) {
     switch action {
     case let .update(feedings):
         // TODO: show all after formatting the list view UI better
+        // This will also reqire better handleing pause/resume/stop reducer actions
         value = feedings.filter { !$0.isFinished }
     }
 }
