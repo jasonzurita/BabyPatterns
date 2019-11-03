e63ed7145d0fa3f9924c9683416822081101de8e
Jason Zurita <jasonzurita@me.com>
2019-11-03 00:22:38 -0400

    chore: update todos

--- Watch Extension/HomeView.swift
+++ Watch Extension/HomeView.swift
@@ -3,6 +3,11 @@ import SwiftUI
 /*
  TODO: - V1 Watch
  - Polish UI
+   + Look at putting nursing and side on left of the feeding chip
+   + Try putting colored circle behind plus circle
+   + Consider laceholder photo behind _no active feeding_ text
+ V2
+ - Have the feeding chip turn into pause and stop buttons instead of popup
  */
 
 struct HomeView: View {
