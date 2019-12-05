032f43f6c86cfeb09a25f32996efaf5c2fb6eeba
Jason Zurita <jasonzurita@me.com>
2019-12-05 17:53:19 -0500

    feat: show error popup if we can't get context
    use the newly added side effects support to
    show a communication failure popup if getting the
    context fails.

--- Watch Extension/State Management/ContextReducer.swift
+++ Watch Extension/State Management/ContextReducer.swift
@@ -4,18 +4,28 @@ import WatchConnectivity
 
 enum ContextAction {
     case requestFullContext
+    case fullContextRequestFailed
 }
 
 // TODO: need to figure out how to remove the state from here since
 // there is no need to have it at all. This reducer is to just formalize
 // an action
-func contextReducer(state _: inout AppState, action: ContextAction) -> [Effect<ContextAction>] {
+func contextReducer(showCommunicationErrorFyiDialog: inout Bool, action: ContextAction) -> [Effect<ContextAction>] {
     switch action {
     case .requestFullContext:
-        let communication = WatchContextCommunication()
-        let encoder = JSONEncoder()
-        guard let data = try? encoder.encode(communication) else { return [] }
-        WCSession.default.sendMessageData(data, replyHandler: nil, errorHandler: nil)
+        return [ { callback in
+            let communication = WatchContextCommunication()
+            let encoder = JSONEncoder()
+            guard let data = try? encoder.encode(communication) else { return }
+            WCSession.default.sendMessageData(data, replyHandler: nil, errorHandler: { _ in
+                callback(.fullContextRequestFailed)
+            })
+        },
+        ]
+    case .fullContextRequestFailed:
+        showCommunicationErrorFyiDialog = true
+        // TODO: we can formalize the removal of the fyi dialog here with a
+        // side effect dispatch async and a new action!
         return []
     }
 }
