dfe46b4df439e46f69bbac184ba87d2f52035556
Jason Zurita <jasonzurita@me.com>
2019-12-05 23:59:55 -0500

    chore: remove stale comment

--- Watch Extension/State Management/ContextReducer.swift
+++ Watch Extension/State Management/ContextReducer.swift
@@ -7,9 +7,6 @@ enum ContextAction {
     case fullContextRequestFailed
 }
 
-// TODO: need to figure out how to remove the state from here since
-// there is no need to have it at all. This reducer is to just formalize
-// an action
 func contextReducer(showCommunicationErrorFyiDialog: inout Bool, action: ContextAction) -> [Effect<ContextAction>] {
     switch action {
     case .requestFullContext:
