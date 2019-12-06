66245227d8b463e329d9e04bb0c4fe4329c4bd55
Jason Zurita <jasonzurita@me.com>
2019-12-06 00:01:22 -0500

    feat: add logging higher order reducer
    for debugging purposes. To use,
    wrap the appReducer in the HomeView
    when building the store: i.e., `logging(appReducer)`

--- Watch Extension/UnidirectionalFlow.swift
+++ Watch Extension/UnidirectionalFlow.swift
@@ -74,3 +74,18 @@ func pullback<GlobalValue, LocalValue, GlobalAction, LocalAction>(
         return globalEffects
     }
 }
+
+func logging<Value, Action>(
+  _ reducer: @escaping Reducer<Value, Action>
+) -> Reducer<Value, Action> {
+  return { value, action in
+    let effects = reducer(&value, action)
+    let newValue = value
+    return [{ _ in
+      print("Action: \(action)")
+      print("Value:")
+      dump(newValue)
+      print("---")
+    }] + effects
+  }
+}
