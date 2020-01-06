2906627af6917497d1f2ded030ce1e28f73066d0
Jason Zurita <jasonzurita@me.com>
2020-01-06 17:19:24 -0500

    feat: make use of `receiveOn` over manual dispatch
    This lets the effect implementation take ownership
    of the queue that an effect turns on over force dispatching.
    
    Also, move Effect type out into separate file for better separation of
    code :).

--- Cycle/Sources/Cycle/Core.swift
+++ Cycle/Sources/Cycle/Core.swift
@@ -1,19 +1,6 @@
 import Foundation
 import Combine
 
-public struct Effect<A> {
-    public let run: (@escaping (A) -> Void) -> Void
-
-    public init(run: @escaping (@escaping (A) -> Void) -> Void) {
-        self.run = run
-    }
-
-    public func map<B>(_ f: @escaping (A) -> B) -> Effect<B> {
-        return Effect<B> { callback in self.run { a in callback(f(a)) }
-        }
-    }
-}
-
 public typealias Reducer<Value, Action> = (inout Value, Action) -> [Effect<Action>]
 
 public final class Store<Value, Action>: ObservableObject {
